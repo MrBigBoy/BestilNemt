7f80adaa39713844845971b4d562684ffa8574fd
Oleksandr <oleksandr.karbovskyy@gmail.com>
2016-10-03 21:36:03 +0200

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class Customer : Person
+    {
+        public DateTime Birthday { get; set; }
+        public Customer(int id, string name, string email, string address, DateTime birthday) : base(id, name, email, address)
+        {
+            this.Birthday = birthday;
+
+        }
+    }
+}
