bfd9386626edbafe9cb60e523659d4a156720b02
Kasper <Kasper@DESKTOP-7SKKK4A>
2016-10-31 12:25:32 +0100

    Test WCF CON

---
+++ BestilNemt/BestilNemtUnitTestTest/WCFReadOnly.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace BestilNemtUnitTestTest
+{
+    [TestClass]
+    public class WCFReadOnly
+    {
+        private BestilNemtServiceRef.BestilNemtServiceClient Proxy = new BestilNemtServiceRef.BestilNemtServiceClient();
+
+  
+        [TestMethod]
+        public void GetShopTestWCF()
+        {
+
+            Shop shop = Proxy.GetShop(1);
+            Assert.AreEqual(1, shop.id); 
+        }
+        [TestMethod]
+        public void GetWarehouseTestWCF()
+        {
+            Warehouse warehouse = Proxy.GetWarehouse(1);
+            Assert.AreEqual(1,warehouse.Id);
+            
+
+        }
+    }
+}
