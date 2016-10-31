3182a941116df19d08cf66b3c8187597725f8c59
DESKTOP-GBO3F5A\Christian <humboshot@gmail.com>
2016-10-31 09:27:10 +0100

    Updated Warehouse

--- BestilNemt/Models/Warehouse.cs
+++ BestilNemt/Models/Warehouse.cs
@@ -21,12 +21,27 @@ namespace Models
         [DataMember]
         public Shop Shop { get; set; }
 
-        public Warehouse()
+        public Warehouse(int id, int stock, int minStock, Product product, Shop shop)
         {
             Id = Id;
             Stock = Stock;
             MinStock = MinStock;
             Products = new List<Product>();
+            Shop = new Shop();
         }
+
+        public Warehouse()
+        {
+            Id = Id;
+            Stock = Stock;
+            MinStock = MinStock;
+        }
+
+        //public Warehouse(int stock, int MinStock)
+        //{
+        //    Stock = Stock;
+        //    MinStock = MinStock;
+        //}
+
     }
 }
