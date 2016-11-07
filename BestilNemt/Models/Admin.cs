6508322a3c3fdc259374e7d529b768eb71928279
Kasper <Kasper@DESKTOP-7SKKK4A>
2016-11-07 09:08:01 +0100

    New Atributes to Admiin and Companyy

--- BestilNemt/Models/Admin.cs
+++ BestilNemt/Models/Admin.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Models
 {
     public class Admin : Person
     {
-        public Admin(int id, string name, string email, string address, Login login, List<Shop> shops , string personType) : base(id, name, email, address, login, shops, personType)
+        [DataMember]
+        public int Membernr { get; set; }
+        public Admin(int id, string name, string email, string address, Login login, List<Shop> shops , string personType,int membernr) : base(id, name, email, address, login, shops, personType)
         {
-
+            Membernr = membernr;
         }
 
         public Admin()
