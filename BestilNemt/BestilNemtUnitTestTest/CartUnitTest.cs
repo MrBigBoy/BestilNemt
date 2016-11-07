c6b2ee2c1bc65ba46430c4a23c7f8150a7938ffb
Oleksandr <oleksandr.karbovskyy@gmail.com>
2016-11-07 13:33:15 +0100

    added CartUnitTest

---
+++ BestilNemt/BestilNemtUnitTestTest/CartUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Controller;
+using Controller.ControllerTestClasses;
+using DataAccessLayer;
+using Models;
+
+namespace BestilNemtUnitTestTest
+{
+    [TestClass]
+    public class CartUnitTest
+    {
+        [TestMethod]
+        public void AddCart()
+        {
+            CartCtr cartCtr = new CartCtr(new CartCtrTestClass());
+            var cart = new Cart();
+            var flag = cartCtr.AddCart(cart);
+            Assert.AreNotEqual(0, flag);
+        }
+
+        [TestMethod]
+        public void FindCart()
+        {
+            CartCtr cartCtr = new CartCtr(new CartCtrTestClass());
+            var cart1 = new Cart(new List<PartOrder>(), 100);
+            var cart2 = new Cart(new List<PartOrder>(), 50);
+            var id1 = cartCtr.AddCart(cart1);
+            var id2 = cartCtr.AddCart(cart2);
+            Assert.IsNotNull(cartCtr.FindCart(id1));
+        }
+
+
+
+
+
+    }
+}
---
+++ BestilNemt/Controller/ControllerTestClasses/CartCtrTestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Models;
+
+namespace Controller.ControllerTestClasses
+{
+    public class CartCtrTestClass : IDbCart
+    {
+        private List<Cart> carts = new List<Cart>();
+        private int idCounter = 1;
+        public int AddCart(Cart cart)
+        {
+            cart.Id = idCounter;
+            carts.Add(cart);
+            idCounter++;
+            return cart.Id;
+        }
+
+        public Cart FindCart(int id)
+        {
+            return carts.FirstOrDefault(c => c.Id == id);
+
+        }
+
+        public List<Cart> GetAllCarts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateCart(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCart(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
