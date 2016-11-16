d4528e8d9c1eb7445c97a0c2b479fffc7d34953c
MSI\MrBigBoy <fg>
2016-11-16 13:29:15 +0100

    Added Warehouse

---
+++ BestilNemt/Models/Warehouse.cs
@@ -0,0 +1,25 @@
+﻿namespace Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        public int MinStock { get; set; }
+        public Product Product { get; set; }
+        public Shop Shop { get; set; }
+
+        public Warehouse()
+        {
+               
+        }
+
+        public Warehouse(int id, int stock, int minStock, Product product, Shop shop)
+        {
+            Id = id;
+            Stock = stock;
+            MinStock = minStock;
+            Product = product;
+            Shop = shop;
+        }
+    }
+}
