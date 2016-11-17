9575f4e3ae8e36d81a8ad1b51098fbd4641c3b68
Christian <humboshot@gmail.com>
2016-11-17 10:09:57 +0100

    Added Saving model class

---
+++ BestilNemt/Models/Saving.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [DataContract]
+    public class Saving
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public double SavingPercent { get; set; }
+
+        public Saving()
+        {
+            StartDate = default(DateTime);
+            EndDate = default(DateTime);
+            SavingPercent = double.NaN;
+        }
+
+        public Saving(int id, DateTime startdate, DateTime enddate, double savingpercent)
+        {
+            Id = id;
+            StartDate = startdate;
+            EndDate = enddate;
+            SavingPercent = savingpercent;
+        }
+
+        public Saving(DateTime startdate, DateTime enddate, double savingpercent)
+        {
+            StartDate = startdate;
+            EndDate = enddate;
+            SavingPercent = savingpercent;
+        }
+    }
+}
