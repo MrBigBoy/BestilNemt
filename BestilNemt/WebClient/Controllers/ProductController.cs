6c4c8f8ba884fefa47d097ec73aaaec96a710de1
Kasper <Kasper@DESKTOP-7SKKK4A>
2016-11-23 09:38:59 +0100

    Add Productcontroller

---
+++ BestilNemt/WebClient/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+           // using(var prox)
+            return View(); 
+        }
+    }
+}
