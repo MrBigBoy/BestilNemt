7856b27e5a523679ede342921c91b6e91397a122
MSI\MrBigBoy <fg>
2016-12-05 09:35:45 +0100

    Added Comments to ProductController and removed SearchProduct and Index from view

--- BestilNemt/WebClient/Controllers/ProductController.cs
+++ BestilNemt/WebClient/Controllers/ProductController.cs
@@ -9,59 +9,63 @@ namespace WebClient.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public Login Login
-        {
-            get
-            {
-                if (Session["Login"] != null)
-                    return (Login)Session["Login"];
-                var login = new Login();
-                Session["Login"] = login;
-                return (Login)Session["Login"];
-            }
-        }
-
+        /**
+         * Get a list of all products by a shop id
+         * The id is Shop id
+         */
         public ActionResult Product(int? id)
         {
             var products = new List<Product>();
+            // if the id is null or has a value lower than 0 return a empty list to the view
             if (id == null || id.Value <= 0)
                 return View(products);
+            // Get the cart from Session
             var cart = (Cart)Session["ShoppingCart"];
             ViewBag.Cart = cart;
             var proxy = new BestilNemtServiceClient();
+            // Get the shop by the id 
             var shop = proxy.GetShop(id.Value);
+            // Get all Warehouses with with the shop id
             shop.Warehouses = proxy.FindAllWarehousesByShopId(id.Value);
+            // Get the shop from session
+            var shopSes = (Shop)Session["Shop"];
+            // If the ids is different reset the cart
+            if (shopSes.Id != shop.Id)
+            {
+                // Clear the cart from Session
+                Session["ShoppingCart"] = new Cart();
+            }
+            // Set the shop
             Session["Shop"] = shop;
-            // Get the products from the warehouses
+            // Get all products from the warehouses
             var listProduct = shop.Warehouses.Select(warehouse => warehouse.Product).ToList();
+            // Return the view with the list of products
             return View(listProduct);
         }
 
+        /**
+         * Return a view of a specific product with a product id
+         * The id is a Product id
+         */
         public ActionResult ProductPage(int? id)
         {
             // If no id is set redirect to frontpage
             if (id == null)
                 return RedirectToAction("Index", "Home");
             var proxy = new BestilNemtServiceClient();
+            // Get the PartOrder with a product by a product id
             var pvm = new ProductPartOrderViewModel { Product = proxy.GetProduct(id.Value) };
+            // Return the view
             return View(pvm);
         }
 
-        public ActionResult SearchProduct(string input)
-        {
-            var proxy = new BestilNemtServiceClient();
-            var productsByName = proxy.FindProductsByName(input);
-            return View(productsByName);
-        }
-
+        /**
+         * Add a product to a cart by a ProductPartOrderViewModel
+         */
         [HttpPost]
         public ActionResult AddProductToCart(ProductPartOrderViewModel partOrder)
         {
+            // Get the login from session
             var login = (Login)Session["Login"];
             var proxy = new BestilNemtServiceClient();
             var cart = (Cart)Session["ShoppingCart"];
