6dba91671a3e2ef221b3e50b3a3532e453ee5945
MSI\MrBigBoy <fg>
2016-12-08 13:10:36 +0100

    Added comments to Cart View

--- BestilNemt/WebClient/Controllers/ShopController.cs
+++ BestilNemt/WebClient/Controllers/ShopController.cs
@@ -6,10 +6,14 @@ namespace WebClient.Controllers
 {
     public class ShopController : Controller
     {
-        /**
-         * Return the ActionResult for all shops by a chain id
-         *  The id is a Chain id
-         */
+        /// <summary>
+        /// Return the ActionResult for all shops by a chain id
+        /// The id is a Chain id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// List of Shops
+        /// </returns>
         public ActionResult ShopList(int? id)
         {
             // If the id is null return a empty list of shops
@@ -23,17 +27,18 @@ namespace WebClient.Controllers
                 proxy.Open();
                 // Get the chain from session
                 var chain = (Chain)Session["Chain"];
-                // If the chain has been set before
-                if (chain != null)
-                {
-                    // If the id is different from the id there has been set before reset the id and clear the cart and the shop id
-                    if (chain.Id != id.Value)
-                    {
-                        Session["Chain"] = proxy.GetChain(id.Value);
-                        Session["ShoppingCart"] = null;
-                        Session["Shop"] = null;
-                    }
-                }
+                // If the chain has not been set before
+                if (chain == null)
+                    return View(proxy.GetAllShopsByChainId(id.Value));
+                // If the id is not different from the id there has been set before reset the id and clear the cart and the shop id
+                if (chain.Id == id.Value)
+                    return View(proxy.GetAllShopsByChainId(id.Value));
+                // Set the new Chain
+                Session["Chain"] = proxy.GetChain(id.Value);
+                // Clear the cart
+                Session["ShoppingCart"] = null;
+                // Clear the shop
+                Session["Shop"] = null;
                 return View(proxy.GetAllShopsByChainId(id.Value));
             }
         }
