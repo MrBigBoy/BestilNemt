541e2c86c9ef97efca2fa813aae82c93112ef85f
MSI\MrBigBoy <fg>
2016-12-09 11:28:20 +0100

    Added comments to Models folder in Web

--- BestilNemt/WebClient/Models/ProductPartOrderViewModel.cs
+++ BestilNemt/WebClient/Models/ProductPartOrderViewModel.cs
@@ -2,11 +2,15 @@
 
 namespace WebClient.Models
 {
+    /// <summary>
+    /// This view model is used to show all products
+    /// </summary>
     public class ProductPartOrderViewModel
     {
         public Product Product { get; set; }
 
         public Cart Cart { get; set; }
+
         public int Amount { get; set; }
 
     }
